594cfe42e46bde902efe31b4eb59929626578f14
oboyle3 <po720999@sju.edu>
2024-05-29 21:17:58 -0400

    sign in

--- flutter_application_1/lib/screens/sign_in.dart
+++ flutter_application_1/lib/screens/sign_in.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({super.key});
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -10,6 +10,45 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add your sign-in logic here
+              },
+              child: Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: SignIn(),
+  ));
 }
